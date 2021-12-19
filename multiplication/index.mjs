e6b3c505756f7db773cdde66ee65257001096918
dabuside <dabuside@gmail.com>
2021-12-19 17:59:42 +0800

    feat: complete karatsuba multiplication

--- multiplication/index.mjs
+++ multiplication/index.mjs
@@ -1,11 +1,27 @@
-function mutiple(a, b) {
-  return a * b;
+function mutiple(x, y) {
+  if (x < 10 && y < 10) {
+    return x * y;
+  }
+  const { a, b, c, d, n, m } = splitNumber(x, y);
+
+  const ac = mutiple(a, c);
+  const bd = mutiple(b, d);
+  const sum = mutiple(a + b, c + d);
+  const sub = sum - ac - bd;
+
+  return ac * 10 ** n + bd + sub * 10 ** m;
 }
 
-function splitNumber(num) {
-  const digital = Math.ceil(Math.log10(num));
-  const base = 10 ** Math.ceil(digital / 2);
-  return [Math.trunc(num / base), num % base];
+function splitNumber(x, y) {
+  const n = Math.ceil(Math.max(Math.log10(x), Math.log10(y)));
+  const m = Math.ceil(n / 2);
+
+  const a = Math.floor(x / 10 ** m);
+  const b = x % 10 ** m;
+
+  const c = Math.floor(y / 10 ** m);
+  const d = y % 10 ** m;
+  return { a, b, c, d, n, m };
 }
 
-export {mutiple, splitNumber};
+export { mutiple, splitNumber };
--- multiplication/test.mjs
+++ multiplication/test.mjs
@@ -1,12 +1,20 @@
 import { mutiple, splitNumber } from "./index.mjs";
 import assert from "assert";
 
-it("should add to numbers from an es module", () => {
+it("mutiple success", () => {
   assert.equal(mutiple(1234, 5678), 7006652);
+  assert.equal(mutiple(1, 5678), 5678);
+  assert.equal(mutiple(0, 5678), 0);
+  assert.equal(mutiple(1, 1), 1);
 });
 
 it("shoule split number currectly", () => {
-  assert.deepEqual(splitNumber(1234), [12, 34]);
-  assert.deepEqual(splitNumber(4567), [45, 67]);
-  assert.deepEqual(splitNumber(123), [1, 23]);
+  assert.deepEqual(splitNumber(5678, 1234), {
+    a: 56,
+    b: 78,
+    c: 12,
+    d: 34,
+    m: 2,
+    n: 4,
+  });
 });

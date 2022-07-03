c245b264cf08cd3ed44af8d24e1307b33f0c4ac0
dabuside <dabuside@gmail.com>
2022-07-03 21:38:26 +0800

    test: add test title

--- count_inversion/test.mjs
+++ count_inversion/test.mjs
@@ -1,9 +1,11 @@
 import count from "./index.mjs";
 import assert from "assert";
 
-it("count inversion currectly", () => {
-  assert.equal(count([1, 3, 5, 2, 4, 6]), 3); //(3,2), (5,2), (5,4)
-  assert.equal(count([]), 0);
-  assert.equal(count([1, 2, 3, 4, 5, 6]), 0);
-  assert.equal(count([6, 5, 4, 3, 2, 1]), 15); // 6*(6*5)/2
+describe("count inversion", () => {
+  it("count inversion currectly", () => {
+    assert.equal(count([1, 3, 5, 2, 4, 6]), 3); //(3,2), (5,2), (5,4)
+    assert.equal(count([]), 0);
+    assert.equal(count([1, 2, 3, 4, 5, 6]), 0);
+    assert.equal(count([6, 5, 4, 3, 2, 1]), 15); // 6*(6*5)/2
+  });
 });
--- multiplication/test.mjs
+++ multiplication/test.mjs
@@ -1,20 +1,22 @@
 import { mutiple, splitNumber } from "./index.mjs";
 import assert from "assert";
 
-it("mutiple success", () => {
-  assert.equal(mutiple(1234, 5678), 7006652);
-  assert.equal(mutiple(1, 5678), 5678);
-  assert.equal(mutiple(0, 5678), 0);
-  assert.equal(mutiple(1, 1), 1);
-});
+describe("multiplication", () => {
+  it("mutiple success", () => {
+    assert.equal(mutiple(1234, 5678), 7006652);
+    assert.equal(mutiple(1, 5678), 5678);
+    assert.equal(mutiple(0, 5678), 0);
+    assert.equal(mutiple(1, 1), 1);
+  });
 
-it("shoule split number currectly", () => {
-  assert.deepEqual(splitNumber(5678, 1234), {
-    a: 56,
-    b: 78,
-    c: 12,
-    d: 34,
-    m: 2,
-    n: 4,
+  it("shoule split number currectly", () => {
+    assert.deepEqual(splitNumber(5678, 1234), {
+      a: 56,
+      b: 78,
+      c: 12,
+      d: 34,
+      m: 2,
+      n: 4,
+    });
   });
 });
